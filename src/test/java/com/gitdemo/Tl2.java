2b5ef94e173de130ecc33ededcde7f96575cddfc
Srinu982 <Kanjulasrinu982@gmail.com>
2025-05-05 12:47:51 +0530

    first commit

---
+++ src/test/java/com/gitdemo/Tl2.java
@@ -0,0 +1,18 @@
+package com.gitdemo;
+
+public class Tl2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//arthematic
+		
+		//addition
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
