fb305d26b05f052d89f8108ed1cc48357d9f967a
Srinu982 <Kanjulasrinu982@gmail.com>
2025-05-05 15:36:15 +0530

    i added one subtraction_sprint class

---
+++ src/test/java/com/gitdemo/Subtraction_sprint.java
@@ -0,0 +1,14 @@
+package com.gitdemo;
+
+public class Subtraction_sprint {
+
+	public static void main(String[] args) {
+		
+		int x=20;
+		int y=10;
+		int z=x-y;
+		System.out.println("subtract of x and y values are:"+z);
+
+	}
+
+}
