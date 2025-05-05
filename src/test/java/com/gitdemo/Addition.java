00ad6cfbf83aaaae1184c760639548360cdf661f
Srinu982 <Kanjulasrinu982@gmail.com>
2025-05-05 16:35:02 +0530

    i added one addition sprint class

---
+++ src/test/java/com/gitdemo/Addition.java
@@ -0,0 +1,14 @@
+package com.gitdemo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=40;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
