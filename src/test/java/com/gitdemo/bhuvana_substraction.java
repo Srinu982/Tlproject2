78fd3669fd6dac3d6a53e174f9c1b45fd2242ec6
Bhuvi15226 <bhuvanapujari5@gmail.com>
2025-05-05 16:21:24 +0530

    I added one substraction_sprint class

---
+++ src/test/java/com/gitdemo/bhuvana_substraction.java
@@ -0,0 +1,14 @@
+package com.gitdemo;
+
+public class bhuvana_substraction {
+
+	public static void main(String[] args) {
+		// Substraction
+		int x=30;
+		int y=10;
+		int z=x-y;
+		System.out.println("Substraction is done" +z);
+
+	}
+
+}
